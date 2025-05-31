d1d27c3f90de5c6ad00d5c4d92cc979ba11dfae1
Michael A. Pedersen <cra7y.com@gmail.com>
2025-05-31 02:03:23 +0200

    Added fixes from issue

--- src/EthernetServer.cpp
+++ src/EthernetServer.cpp
@@ -25,7 +25,7 @@
 uint16_t EthernetServer::server_port[MAX_SOCK_NUM];
 
 
-void EthernetServer::begin()
+void EthernetServer::begin(uint16_t port_not_used)
 {
 	uint8_t sockindex = Ethernet.socketBegin(SnMR::TCP, _port);
 	if (sockindex < MAX_SOCK_NUM) {
